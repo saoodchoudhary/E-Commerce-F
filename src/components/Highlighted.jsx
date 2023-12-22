ea43955ed13bb3647f597b2f75629e948a2c9c3f
Docs in Hindi <docsinhindi@gmail.com>
2023-12-22 17:57:28 +0530

    fix : highlighted page fix

--- src/components/Highlighted.jsx
+++ src/components/Highlighted.jsx
@@ -12,36 +12,36 @@ const Highlighted = () => {
                 <div className='flex justify-center flex-1 gap-2 border-r-[1px]'>
                     <FontAwesomeIcon className=' self-center text-blue-500' icon={faShippingFast} />
                     <div>
-                        <h3 className=' font-semibold'>Free Delivery</h3>
+                        <h3 className=' font-semibold text-center'>Free Delivery</h3>
                         <p className=" text-center">from $50</p>
                     </div>
                 </div>
                 <div className='flex justify-center flex-1 gap-2 border-r-[1px]'>
                     <FaRegThumbsUp className='self-center text-blue-500' />
                     <div>
-                        <h3 className=' font-semibold'>Free Delivery</h3>
-                        <p className=" text-center">from $50</p>
+                        <h3 className=' font-semibold text-center'>99% Positive</h3>
+                        <p className=" text-center">Feedbacks</p>
                     </div>
                 </div>
                 <div className='flex justify-center flex-1 gap-2 border-r-[1px]'>
                     <MdOutlineCurrencyExchange className='self-center text-blue-500' />
                     <div>
-                        <h3 className=' font-semibold'>Free Delivery</h3>
-                        <p className=" text-center">from $50</p>
+                        <h3 className=' font-semibold text-center'>365 days</h3>
+                        <p className=" text-center">for free return</p>
                     </div>
                 </div>
                 <div className='flex justify-center flex-1 gap-2 border-r-[1px]'>
                     <RiSecurePaymentFill className='self-center text-blue-500' />
                     <div>
-                        <h3 className=' font-semibold'>Free Delivery</h3>
-                        <p className=" text-center">from $50</p>
+                        <h3 className=' font-semibold text-center'>Payment</h3>
+                        <p className=" text-center">Secure System</p>
                     </div>
                 </div>
                 <div className='flex justify-center flex-1 gap-2'>
                     <GiPin className='self-center text-blue-500' />
                     <div>
-                        <h3 className=' font-semibold'>Free Delivery</h3>
-                        <p className=" text-center">from $50</p>
+                        <h3 className=' font-semibold text-center'>Only Best</h3>
+                        <p className=" text-center">Brands</p>
                     </div>
                 </div>
             </div>
